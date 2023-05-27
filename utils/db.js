ba32884b8f7cbe091349d74ed9d5b10ec87721cd
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-27 11:27:51 +0000

    task 1

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const {
+      DB_HOST = 'localhost',
+      DB_PORT = '27017',
+      DB_DATABASE = 'files_manager',
+    } = process.env;
+    const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+    console.log(url);
+    this.client = new MongoClient(url);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
